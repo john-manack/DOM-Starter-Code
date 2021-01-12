fc2310c507af6e0b3c4e34417fda2d92b937de8f
John Manack <jmnck28@gmail.com>
2021-01-11 20:07:34 -0500

    updated html and js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 'use strict';
+
+console.log("Sweet, black-tar, Columbian JS loaded.");
+
+const generateMadlib = document.querySelector("#generateMadlib");
+
+generateMadlib.addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Event functions
+    const inputSelectors = document.querySelectorAll('input');
+    const foodResponse = document.querySelector('#foodResponse');
+    const drinkResponse = document.querySelector('#drinkResponse');
+
+    inputSelectors.forEach(function (inputItem) {
+        if (inputItem.name === 'favFood') {
+            foodResponse.innerHTML = inputItem.value;
+        }
+        if (inputItem.name === 'favDrink') {
+            drinkResponse.innerHTML = inputItem.value;
+        }
+    });
+});
